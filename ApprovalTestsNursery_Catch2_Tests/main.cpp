dc9691b0175770d39ddb8d7de5aa9479ac99b2a8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-04-22 19:04:56 +0100

    Automatically use diff or fc as reporter in CI builds. Behaviour unchanged for all other builds.

--- ApprovalTestsNursery_Catch2_Tests/main.cpp
+++ ApprovalTestsNursery_Catch2_Tests/main.cpp
@@ -4,7 +4,18 @@
 #define APPROVALS_CATCH
 #include "ApprovalTests/Catch2Approvals.h"
 #include "ApprovalTests/Approvals.h"
+#include "CIBuildOnlyReporter.h"
+#include "TextDiffReporter.h"
 
 auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
 
-// TODO If it's a CI build, don't allow GUI applications to pop up.
+// If it's a CI build, don't allow GUI applications to pop up.
+// If it's a CI machine
+//      use ConsoleDiffReporter - with 'C:\Windows\System32\fc.exe' or 'diff'
+//      return true
+// else
+//      return false
+auto reporterDisposer = 
+    Approvals::useAsFrontLoadedReporter (
+        std::make_shared<CIBuildOnlyReporter>(
+            std::make_shared<TextDiffReporter>() ) );

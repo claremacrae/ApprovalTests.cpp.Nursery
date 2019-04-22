0bc4e1acb85b719b76dd526c4a576e1d73324b4a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-04-22 19:11:15 +0100

    For non-CI build, use QuietReporter, to stop delibarate failures being annoying

--- ApprovalTestsNursery_Catch2_Tests/main.cpp
+++ ApprovalTestsNursery_Catch2_Tests/main.cpp
@@ -4,18 +4,21 @@
 #define APPROVALS_CATCH
 #include "ApprovalTests/Catch2Approvals.h"
 #include "ApprovalTests/Approvals.h"
+#include "ApprovalTests/reporters/QuietReporter.h"
 #include "CIBuildOnlyReporter.h"
 #include "TextDiffReporter.h"
 
 auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
 
-// If it's a CI build, don't allow GUI applications to pop up.
 // If it's a CI machine
 //      use ConsoleDiffReporter - with 'C:\Windows\System32\fc.exe' or 'diff'
-//      return true
-// else
-//      return false
-auto reporterDisposer = 
+auto reporterForCIDisposer = 
     Approvals::useAsFrontLoadedReporter (
         std::make_shared<CIBuildOnlyReporter>(
             std::make_shared<TextDiffReporter>() ) );
+
+// Otherwise, for developer builds, use QuietReporter, because we have some
+// tests that deliberately report failures.
+auto interactiveReporterDisposer =
+    Approvals::useAsDefaultReporter (
+        std::make_shared<QuietReporter>() );

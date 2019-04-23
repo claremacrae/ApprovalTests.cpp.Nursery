ed368d764ea38fadce656bf34cbea1a6a0ad00db
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-04-23 08:32:31 +0100

    Formatting to try to improve readability

--- ApprovalTestsNursery_Catch2_Tests/main.cpp
+++ ApprovalTestsNursery_Catch2_Tests/main.cpp
@@ -11,14 +11,12 @@
 auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
 
 // If it's a CI machine, show differences as text in the console/log
-auto reporterForCIDisposer = 
-    Approvals::useAsFrontLoadedReporter (
-        std::make_shared<CIBuildOnlyReporter>(
-            std::make_shared<TextDiffReporter>() ) );
+auto reporterForCIDisposer = Approvals::useAsFrontLoadedReporter (
+    std::make_shared<CIBuildOnlyReporter>(
+        std::make_shared<TextDiffReporter>() ) );
 
 // Demonstrate how to set a default reporter for tests on non-CI machines.
 // (in fact, DiffReporter is the default anyway, so this doesn't actually
 // change the behaviour)
-auto interactiveReporterDisposer =
-    Approvals::useAsDefaultReporter (
-        std::make_shared<DiffReporter>() );
+auto interactiveReporterDisposer = Approvals::useAsDefaultReporter (
+    std::make_shared<DiffReporter>() );

44c22be4d73492113e664ea4daee48fddbfdfbde
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-08 15:06:20 +0100

    F Add CombinationApprovalsOld - straight copy of CombinationApprovals to allow for experimenting with the implementation

---
+++ ApprovalTestsNursery_Catch2_Tests/CombinationApprovalsOldTests.cpp
@@ -0,0 +1,68 @@
+#include "Catch.hpp"
+#include <map>
+#include <vector>
+#include <string>
+#include "PairUtilities.h"
+#include "CombinationApprovalsOld.h"
+#include "reporters/FakeReporter.h"
+
+TEST_CASE("YouCanVerifyCombinationsOf1") {
+    std::vector<std::string> words{"hello", "world"};
+    CombinationApprovalsOld::verifyAllCombinations( [](std::string s){return s + "!";}, words);
+}
+
+TEST_CASE("YouCanVerifyCombinationsOf1WithTemplateParameters") {
+    std::vector<std::string> words{"hello", "world"};
+    CombinationApprovalsOld::verifyAllCombinations<std::vector<std::string>, std::string>( [](std::string s){return s + "!";}, words);
+}
+
+TEST_CASE("YouCanVerifyCombinationsOf1Reports") {
+    std::vector<std::string> words{"hello", "world"};
+    FakeReporter reporter;
+    try
+    {
+        CombinationApprovalsOld::verifyAllCombinations( [](std::string s){return s + "!";}, words, reporter);
+    }
+    catch(const ApprovalException&)
+    {
+        // ignore
+    }
+    REQUIRE(reporter.called == true);
+}
+
+// begin-snippet: YouCanVerifyCombinationsOf2
+TEST_CASE("YouCanVerifyCombinationsOf2") {
+    std::vector<std::string> v{"hello", "world"};
+    std::vector<int> numbers{1, 2, 3};
+    CombinationApprovalsOld::verifyAllCombinations<
+        std::vector<std::string>, std::vector<int>, std::pair<std::string, int>>(
+            [](std::string s, int i){return std::make_pair(s, i);},
+            v,
+            numbers);
+}
+// end-snippet
+
+TEST_CASE("YouCanVerifyCombinationsOf9") {
+    std::vector<std::string> letters{"a", "b"};
+    CombinationApprovalsOld::verifyAllCombinations<
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::string>( [](
+            std::string s1,
+            std::string s2,
+            std::string s3,
+            std::string s4,
+            std::string s5,
+            std::string s6,
+            std::string s7,
+            std::string s8,
+            std::string s9)
+                              {return s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9;}, letters, letters, letters, letters, letters, letters, letters, letters, letters);
+}

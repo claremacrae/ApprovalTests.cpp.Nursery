a7dec38915ce0f52b306f9b4295eaef202b8e879
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-08 16:05:56 +0100

    t Improve test coverage of combinations further

--- ApprovalTestsNursery_Catch2_Tests/CombinationApprovalsOldTests.cpp
+++ ApprovalTestsNursery_Catch2_Tests/CombinationApprovalsOldTests.cpp
@@ -98,3 +98,42 @@ TEST_CASE("CombinationOf3WithLambda") {
             std::string s3)
                               {return s1 + s2 + s3;}, {"a"}, {"b"}, {"c"});
 }
+
+std::string concatenate1(std::string s1)
+{
+    return s1;
+}
+
+std::string concatenate2(std::string s1, std::string s2)
+{
+    return s1 + s2;
+}
+
+std::string concatenate3(
+            std::string s1,
+            std::string s2,
+            std::string s3)
+{
+    return s1 + s2 + s3;
+}
+
+TEST_CASE("CombinationOf1WithFunction") {
+    CombinationApprovalsOld::verifyAllCombinations<
+            std::vector<std::string>,
+            std::string>(concatenate1, {"a"});
+}
+
+TEST_CASE("CombinationOf2WithFunction") {
+    CombinationApprovalsOld::verifyAllCombinations<
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::string>(concatenate2, {"a"}, {"b"});
+}
+
+TEST_CASE("CombinationOf3WithFunction") {
+    CombinationApprovalsOld::verifyAllCombinations<
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::vector<std::string>,
+            std::string>(concatenate3, {"a"}, {"b"}, {"c"});
+}

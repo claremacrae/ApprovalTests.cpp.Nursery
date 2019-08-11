294ed3473f6961db589fb21cf7ceafa7c4abcd8f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-11 12:29:32 +0100

    F add code for experimenting with Catch2's generators

---
+++ ApprovalTestsNursery_Catch2_Tests/CatchGeneratorExperiments.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Catch.hpp"
+
+// A place to experiment with Catch2' generators, and debug them,
+// to get ideas for alternative implementations of our CombinationApprovals
+
+TEST_CASE("Generator1")
+{
+    auto i = GENERATE(1, 2, 3);
+    SECTION("one") {
+        auto j = GENERATE( -3, -2, -1 );
+        std::cout << i << ", " << j << '\n';
+    }
+}
+
+TEST_CASE("Generator2")
+{
+    // Interesting: sections are not needed - the following does print out all
+    // combinations of i, j and k
+    auto i = GENERATE(1, 2);
+
+    // auto j = GENERATE(std::string("hello"), std::string("world"));
+    auto j = GENERATE(as<std::string>{}, "hello", "world");
+
+    // TODO Check docs for range - is it clear that last value is not included?
+    // TODO Docs don't make it clear that step can be negative 
+    auto k = GENERATE(range(7,10));
+    std::cout <<
+        i << ", " <<
+        j << ", " <<
+        k << 
+        '\n';
+}

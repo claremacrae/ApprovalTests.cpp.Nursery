ddba0aea2068fb8e896aea5156f92f2e62bcae4e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-21 15:10:23 +0100

    r Inline ForegroundSystemLauncher, ready for moving to main project

--- ApprovalTestsNursery/ForegroundSystemLauncher.h
+++ ApprovalTestsNursery/ForegroundSystemLauncher.h
@@ -11,7 +11,26 @@
 class ForegroundSystemLauncher : public CommandLauncher
 {
 public:
-    bool launch(std::vector<std::string> argv) override;
+    bool launch(std::vector<std::string> argv) override
+    {
+        SystemLauncher temp_launcher;
+        if (!temp_launcher.exists(argv.front()))
+        {
+            return false;
+        }
+
+        // Surround each of the arguments by double-quotes:
+        const std::string command = std::accumulate(
+            argv.begin(), argv.end(), std::string(""),
+            [](std::string a, std::string b) {return a + " " + "\"" + b + "\""; });
+
+        // See https://stackoverflow.com/a/9965141/104370 for why the Windows string is so complex:
+        const std::string launch = SystemUtils::isWindowsOs() ?
+            (std::string("cmd /S /C ") + "\"" + command + "\"") :
+            (command);
+        system(launch.c_str());
+        return true;
+    }
 };
 
 #endif //APPROVALTESTS_CPP_FOREGROUNDSYSTEMLAUNCHER_H

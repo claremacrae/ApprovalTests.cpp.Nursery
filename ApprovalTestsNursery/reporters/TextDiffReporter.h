6eb96dd1463fa6a65709c2da5a00e7bc70844e2b
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-21 15:12:17 +0100

    r Inline TextDiffReporter, ready for moving to main project

--- ApprovalTestsNursery/reporters/TextDiffReporter.h
+++ ApprovalTestsNursery/reporters/TextDiffReporter.h
@@ -2,6 +2,8 @@
 #define APPROVALTESTS_CPP_NURSERY_TEXTDIFFREPORTER_H
 
 #include "reporters/Reporter.h"
+#include "ConsoleDiffReporter.h"
+#include "reporters/FirstWorkingReporter.h"
 
 #include <memory>
 
@@ -14,8 +16,27 @@ class TextDiffReporter : public Reporter
 private:
     std::unique_ptr<Reporter> m_reporter;
 public:
-    TextDiffReporter();
-    bool report(std::string received, std::string approved) const override;
+    TextDiffReporter()
+    {
+        std::vector<Reporter*> reporters = {
+            new ConsoleDiffReporter("diff"),
+            new ConsoleDiffReporter("C:/Windows/System32/fc.exe")
+        };
+        m_reporter = std::unique_ptr<Reporter>(new FirstWorkingReporter(reporters));
+    }
+    bool report(std::string received, std::string approved) const override
+    {
+        std::cout << "Comparing files:" << std::endl;
+        std::cout << "received: " << received << std::endl;
+        std::cout << "approved: " << approved << std::endl;
+        const bool result = m_reporter->report(received, approved);
+        if (!result)
+        {
+            std::cout << "TextDiffReporter did not find a working diff program\n\n";
+        }
+
+        return result;
+    }
 };
 
 #endif //APPROVALTESTS_CPP_NURSERY_TEXTDIFFREPORTER_H

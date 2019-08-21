dbabf9cf426b48d6a704966948d336b3c91a001c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-21 15:06:23 +0100

    r Inline FileUtilsAdditions, ready for moving to main project

--- ApprovalTestsNursery/FileUtilsAdditions.h
+++ ApprovalTestsNursery/FileUtilsAdditions.h
@@ -1,13 +1,42 @@
 #ifndef APPROVALTESTS_CPP_NURSERY_FILEUTILSADDITIONS_H
 #define APPROVALTESTS_CPP_NURSERY_FILEUTILSADDITIONS_H
 
+#include "FileUtils.h"
+
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
 #include <string>
 
 class FileUtilsAdditions
 {
 public:
-    static std::string readFileThrowIfMissing(std::string fileName);
-    static std::string readFileReturnEmptyIfMissing(std::string fileName);
+    static std::string readFileThrowIfMissing(std::string fileName)
+    {
+        std::ifstream in(fileName.c_str(), std::ios_base::in);
+        if (!in)
+        {
+            throw std::runtime_error("File does not exist: " + fileName);
+        }
+        std::stringstream written;
+        written << in.rdbuf();
+        in.close();
+
+        std::string text = written.str();
+        return text;
+    }
+
+    static std::string readFileReturnEmptyIfMissing(std::string fileName)
+    {
+        if (FileUtils::fileExists(fileName))
+        {
+            return readFileThrowIfMissing(fileName);
+        }
+        else
+        {
+            return std::string();
+        }
+    }
 };
 
 #endif //APPROVALTESTS_CPP_NURSERY_FILEUTILSADDITIONS_H

a058d67b45b96a4106f0a08201611b0f7b3faa65
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-08-21 14:53:55 +0100

    r Inline CatchReporter, ready for moving to main project

--- ApprovalTestsNursery/reporters/CatchReporter.h
+++ ApprovalTestsNursery/reporters/CatchReporter.h
@@ -2,12 +2,32 @@
 #define APPROVALTESTS_CPP_NURSERY_CATCHREPORTER_H
 
 #include "reporters/Reporter.h"
+#include "FileUtilsAdditions.h"
+
+#include <iostream>
+
+// TODO Need to work out how to include this in a release, without
+//      giving a compilation error for those using other test frameworks
+// Could check if Catch header has already been included, by checking
+// for the Catch version number #defines, but that would only work if
+// the Catch header was #included before the Approval Tests single-header
+#include "Catch.hpp"
 
 // A reporter which uses Catch CHECK statement to check the output
 class CatchReporter : public Reporter
 {
 public:
-    bool report(std::string received, std::string approved) const override;
+    bool report(std::string received, std::string approved) const override
+    {
+        const auto receivedText = FileUtilsAdditions::readFileReturnEmptyIfMissing(received);
+        const auto approvedText = FileUtilsAdditions::readFileReturnEmptyIfMissing(approved);
+        // By using CHECK instead of REQUIRE, program flow continues - Catch does not throw an exception
+        std::cout << "Checking content of files:" << std::endl;
+        std::cout << "received: " << received << std::endl;
+        std::cout << "approved: " << approved << std::endl;
+        CHECK(receivedText == approvedText);
+        return true;
+    }
 };
 
 

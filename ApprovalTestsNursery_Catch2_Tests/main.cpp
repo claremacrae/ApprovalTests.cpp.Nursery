9f2266553337448743eadff696a042ffd35d9309
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-09-04 09:53:48 +0100

    F Remove CIBuildOnlyReporter as it is now in the main project

--- ApprovalTestsNursery_Catch2_Tests/main.cpp
+++ ApprovalTestsNursery_Catch2_Tests/main.cpp
@@ -5,14 +5,14 @@
 #include "Catch2Approvals.h"
 #include "ApprovalTests/Approvals.h"
 #include "ApprovalTests/reporters/DiffReporter.h"
-#include "reporters/CIBuildOnlyReporter.h"
+//#include "reporters/CIBuildOnlyReporter.h"
 #include "reporters/TextDiffReporter.h"
 
 auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
 
 using namespace ApprovalTests;
 // If it's a CI machine, show differences as text in the console/log
-auto ciReporterDisposer = CIBuildOnlyReporter::useAsFrontLoadedReporter( std::make_shared<TextDiffReporter>() );
+//auto ciReporterDisposer = CIBuildOnlyReporter::useAsFrontLoadedReporter( std::make_shared<TextDiffReporter>() );
 
 // Demonstrate how to set a default reporter for tests on non-CI machines.
 // (in fact, DiffReporter is the default anyway, so this doesn't actually

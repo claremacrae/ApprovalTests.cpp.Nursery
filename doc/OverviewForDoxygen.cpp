08e747ff11a3feb8f44a7af580a8eeef703d1f47
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-05-01 16:33:01 +0100

    d Add a trivial Doxygen home page

---
+++ doc/OverviewForDoxygen.cpp
@@ -0,0 +1,14 @@
+/*! \mainpage ApprovalTests.cpp.Nursery
+
+ Purpose of document: Experiment with Doxygen and Sphinx configuration,
+ in preparation for improving ApprovalTests.cpp docs.
+
+ \section contents Contents
+
+ - \ref overview
+
+ \section overview Overview
+
+ A place to experiment with ideas for [ApprovalTests.cpp](https://github.com/approvals/ApprovalTests.cpp)
+
+ */

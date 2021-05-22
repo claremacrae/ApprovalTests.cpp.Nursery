c809327556b5f80396973c2ab3e66fe435cf9ad8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-05-22 19:23:22 +0100

    t Show use of Approvals::verify with Catch2 generators

--- ApprovalTestsNursery_Catch2_Tests/CatchGeneratorExperiments.cpp
+++ ApprovalTestsNursery_Catch2_Tests/CatchGeneratorExperiments.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include "catch2/catch.hpp"
 
+#include "Approvals.h"
+#include "reporters/AutoApproveReporter.h"
+
+using namespace ApprovalTests;
+
 // A place to experiment with Catch2' generators, and debug them,
 // to get ideas for alternative implementations of our CombinationApprovals
 
 TEST_CASE("Generator1")
 {
     auto i = GENERATE(1, 2, 3);
-    SECTION("one")
+    SECTION(std::to_string(i))
     {
         auto j = GENERATE(-3, -2, -1);
         std::cout << i << ", " << j << '\n';
+        SECTION(std::to_string(j))
+        {
+            Approvals::verify(std::to_string(i) + " " + std::to_string(j));
+        }
     }
 }
 
